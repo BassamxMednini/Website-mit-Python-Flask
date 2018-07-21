20dadd1aa4b95c3aad78429c1126c6eda4ab6968
Bassam <Bassam.Mednini.1997@gmail.com>
2018-07-21 15:17:55 +0200

    Enthält mehrere Artikel mit unterschiedlichen IDs, die über eine For-Schleife angezeigt wird

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti accusamus aperiam, at perspiciatis earum autem, fugit, ipsum ipsa quidem magnam commodi! Reprehenderit soluta praesentium laboriosam placeat quo nostrum ratione repudiandae!',
+            'author': 'Bassam Mednini',
+            'create_day': '21-07-2017'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti accusamus aperiam, at perspiciatis earum autem, fugit, ipsum ipsa quidem magnam commodi! Reprehenderit soluta praesentium laboriosam placeat quo nostrum ratione repudiandae!',
+            'author': 'Bassam Mednini',
+            'create_day': '21-07-2017'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti accusamus aperiam, at perspiciatis earum autem, fugit, ipsum ipsa quidem magnam commodi! Reprehenderit soluta praesentium laboriosam placeat quo nostrum ratione repudiandae!',
+            'author': 'Bassam Mednini',
+            'create_day': '21-07-2017'
+        }
+    ]
+    return articles
