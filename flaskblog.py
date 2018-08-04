a217d3e3c337d567ee665ee97f09acf11e569678
Bassam <Bassam.Mednini.1997@gmail.com>
2018-08-04 19:39:27 +0200

    2 neue Funktionen + importieren von 'forms' & erstellen eines secret_key

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '795b0dbb2ca1ae607d71bbe347d4f126'
 
 posts = [
     {
@@ -11,18 +14,6 @@ posts = [
     {
         'author': 'Mednini Bassam',
         'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'July 29, 2018'
-    },
-    {
-        'author': 'Mednini Bassam',
-        'title': 'Blog Post 3',
-        'content': 'Second post content',
-        'date_posted': 'July 29, 2018'
-    },
-    {
-        'author': 'Mednini Bassam',
-        'title': 'Blog Post 4',
         'content': 'Second post content',
         'date_posted': 'July 29, 2018'
     }
@@ -37,6 +28,17 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
+
 
 # Debug-Modus aktivieren
 if __name__ == '__main__':
