4044752dcfc4d7afed512f8f9aa0b533e9db27a5
Bassam <Bassam.Mednini.1997@gmail.com>
2018-08-06 23:46:45 +0200

    Klasse 'User' erstellt

--- flaskblog.py
+++ flaskblog.py
@@ -7,7 +7,15 @@ app.config['SECRET_KEY'] = '795b0dbb2ca1ae607d71bbe347d4f126'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqllite:///site.db'
 db = SQLAlchemy(app)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    username = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
 
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}', '{self.username}')"
 
 posts = [
     {
