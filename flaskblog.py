ea0e20a6db344cd3d428416ca2402458af72bde1
Bassam <Bassam.Mednini.1997@gmail.com>
2018-08-08 23:56:57 +0200

    Test für Datenbank

--- flaskblog.py
+++ flaskblog.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
-from models import User, Post
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '795b0dbb2ca1ae607d71bbe347d4f126'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
+from models import User, Post
 
 posts = [
     {
