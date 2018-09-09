2d2569794096b9ed3331cd4cc2cf2b452e3f7bf7
Bassam <Bassam.Mednini.1997@gmail.com>
2018-09-09 23:22:09 +0200

    Dateien gelöscht

--- flaskblog/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '795b0dbb2ca1ae607d71bbe347d4f126'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-
-from flaskblog import routes
