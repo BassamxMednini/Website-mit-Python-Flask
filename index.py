c5ef6f79f86bc8a1f3a30ca553c05199e5087240
Bassam Mednini <Bassam.Mednini.1997@gmail.com>
2018-09-17 21:10:20 +0200

    Liste erstellt, dass ein Dictionary enthält

--- index.py
+++ index.py
@@ -1,7 +1,22 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Bassam Mednini',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': '17. September 2018'
+    },
+    {
+        'author': 'Max Mustermann',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': '16. September 2018'
+    }
+]
+
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
