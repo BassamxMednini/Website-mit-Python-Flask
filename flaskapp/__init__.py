7526a4b1080a52b265715a7c2942bb033f9d1208
Bassam Mednini <Bassam.Mednini.1997@gmail.com>
2018-10-03 18:32:06 +0200

    Enthält die "wichtigsten Elemente"

---
+++ flaskapp/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+##########################
+# Aufgrund dem Namen '__init__' ist diese Datei die sogenannte 'Erste Datei',
+# die bei einem 'import flaskapp from ...' kontrolliert wird
+##########################
+
+
+# Ausführen der Applikation
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '2b4311fdf854371c367d26a90e81b2d0'
+# Datenbank erstellen
+# Das **from flask_sqlalchemy import SQLAlchemy** gehört ebenfalls dazu
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+from flaskapp import routes
