720f30f88081ec1a45d250bc7a0ef6d9dc16c54d
Bassam Mednini <Bassam.Mednini.1997@gmail.com>
2018-10-08 21:46:52 +0200

    Login_manager importiert

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 ##########################
@@ -19,5 +20,8 @@ db = SQLAlchemy(app)
 
 # Passwörter werden geschlüsselt
 bcrypt = Bcrypt(app)
+
+#Login
+login_manager = LoginManager(app)
 
 from flaskapp import routes
