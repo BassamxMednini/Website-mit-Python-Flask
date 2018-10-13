889b66ea01ba9d72c30990b35144816ebb64a7a4
Bassam Mednini <Bassam.Mednini.1997@gmail.com>
2018-10-13 20:15:49 +0200

    UpdateAccountForm zum aktualisieren des eigenen Profils

--- flaskapp/forms.py
+++ flaskapp/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskapp.models import User
@@ -29,3 +30,21 @@ class LoginForm(FlaskForm):
     remember = BooleanField('Passwort speichern')
     submit = SubmitField('Anmelden')
 
+# Ändern des eigenen Profils
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Benutzername', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Aktualisieren')
+
+    # Überprüft, ob Username in der Datenbank bereits existiert
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('Dieser Benutzername ist leider bereits vergeben. Bitte suche dir einen neuen Name aus.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('Diese Email ist leider bereits vergeben. Bitte suche dir eine neue Email aus.')
