ac3f1cdd9cd2df076c578b8ef1684dae96daaf37
Bassam Mednini <Bassam.Mednini.1997@gmail.com>
2018-10-17 22:43:15 +0200

    Maximalgröße für PB festgelegt

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -1,5 +1,6 @@
 import os
 import secrets
+from PIL import Image
 from flask import render_template, flash, redirect, url_for, request
 from flaskapp import app, db, bcrypt
 from flaskapp.forms import RegistrationForm, LoginForm, UpdateAccountForm
@@ -91,7 +92,12 @@ def save_picture(form_picture):
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
     picture_path = os.path.join(app.root_path, 'static/profile_images/', picture_fn)
-    form_picture.save(picture_path)
+
+    # Maximalgröße für PB  
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
 
     return picture_fn
 
